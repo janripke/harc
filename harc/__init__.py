5b1f312e2f8840bb674259b63159aa124d097a38
Jan Ripke <jan.ripke@nl.abnamro.com>
2019-08-30 09:23:26 +0200

    some correction on onno's changes

--- harc/__init__.py
+++ harc/__init__.py
@@ -2,11 +2,11 @@ __version__ = '1.0.27-SNAPSHOT'
 
 __title__ = 'harc'
 __description__ = 'Harc equals Hit And Release Code using cloud providers'
-__uri__ = 'https://github.com/janripke/harc-server.git'
+__uri__ = 'https://github.com/janripke/harc.git'
 __doc__ = __description__ + ' <' + __uri__ + '>'
 
 __author__ = 'Jan Ripke'
-__email__ = 'jan.ripke@harcmail.com'
+__email__ = 'janripke@gmail.com'
 
 __license__ = 'GNU General Public License v3 or later (GPLv3+)'
 __copyright__ = 'Copyleft (l) 2017-2019 Jan Ripke'
