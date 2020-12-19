103dc0c8d31299c061f89e8a9c101109c7b0651c
Jan Ripke <janripke@gmail.com>
2020-12-20 00:13:06 +0100

    use shell.command

--- harc/system/git.py
+++ harc/system/git.py
@@ -10,11 +10,15 @@ def clone(repository, folder):
 
 
 def branches(folder):
-    statement = "cd " + folder + ";" + "git ls-remote --heads origin"
-    p = Popen([statement], stdout=PIPE, shell=True)
-    output, error = p.communicate()
-    if p.returncode != 0:
-        raise PluginException(error)
+    # statement = "cd " + folder + ";" + "git ls-remote --heads origin"
+    statement = "cd {};git ls-remote --heads origin"
+    output = command.execute(statement)
+    output = command.stringify(output)
+
+    # p = Popen([statement], stdout=PIPE, shell=True)
+    # output, error = p.communicate()
+    # if p.returncode != 0:
+    #     raise PluginException(error)
 
     output = output.decode('utf-8')
     lines = output.split("\n")
@@ -26,8 +30,15 @@ def branches(folder):
 
 
 def commit(version, folder):
-    # statement = "cd " + folder + ";" + "git commit -a --message='updated to version " + version + "'"
     statement = "cd {};git commit -a --message='updated to version {}'".format(folder, version)
+    output = command.execute(statement)
+    return command.stringify(output)
+
+
+def tag(version, folder):
+    # statement = "cd " + folder + ";" + "git tag -a " + version + " -m '" + version + "'"
+
+    statement = "cd {}; git tag -a {} -m '{}'".format(folder, version, version)
     output = command.execute(statement)
     return command.stringify(output)
 
@@ -38,19 +49,16 @@ def commit(version, folder):
     # return output
 
 
-def tag(version, folder):
-    statement = "cd " + folder + ";" + "git tag -a " + version + " -m '" + version + "'"
-    p = Popen([statement], stdout=PIPE, shell=True)
-    output, error = p.communicate()
-    if p.returncode != 0:
-        raise PluginException(error)
-    return output
-
-
 def push(repository, branch, folder):
-    statement = "cd " + folder + ";" + "git push --tags " + repository + " " + branch
-    p = Popen([statement], stdout=PIPE, shell=True)
-    output, error = p.communicate()
-    if p.returncode != 0:
-        raise PluginException(error)
-    return output
+    # statement = "cd " + folder + ";" + "git push --tags " + repository + " " + branch
+
+    statement = "cd {};git push --tags {} {}".format(folder, repository, branch)
+    output = command.execute(statement)
+    return command.stringify(output)
+
+
+    # p = Popen([statement], stdout=PIPE, shell=True)
+    # output, error = p.communicate()
+    # if p.returncode != 0:
+    #     raise PluginException(error)
+    # return output
