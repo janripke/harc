7f16ecd48092ef833f2b05616b5d92253af07f5b
Devops <Devops>
2022-03-10 20:11:12 +0100

    DevopsRelease

--- harc/plugins/plugin_command.py
+++ harc/plugins/plugin_command.py
@@ -24,3 +24,6 @@ class PluginCommand(click.MultiCommand):
         if cmd_name == "devops:release":
             from harc.plugins.azure.devops_release import DevopsRelease
             return DevopsRelease.execute
+        if cmd_name == "devops:publish":
+            from harc.plugins.azure.devops_publish import DevopsPublish
+            return DevopsPublish.execute
