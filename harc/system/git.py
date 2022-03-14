174b4f41a958d22c4d87c9103c1208babd4d3bda
Devops <Devops>
2022-03-14 12:34:28 +0100

    devops

--- harc/system/git.py
+++ harc/system/git.py
@@ -69,7 +69,8 @@ def push_tags(branch: str, folder=None):
     if folder:
         output = command.execute(f"cd {folder};git push --tags origin {branch}")
     else:
-        output = command.execute(f"git push -u origin {branch} --tags")
+        # output = command.execute(f"git push -u origin {branch} --tags")
+        output = command.execute(f"git push --tags -u origin {branch}")
     return command.stringify(output)
 
 
